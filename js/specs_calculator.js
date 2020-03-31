f029b9b6c90902f40343846f329fa695a65a48f3
GrowerCH <58558080+GrowerCH@users.noreply.github.com>
2020-03-31 15:38:25 +0200

    refactor

--- js/specs_calculator.js
+++ js/specs_calculator.js
@@ -1,3 +1,23 @@
+const SPEC_LIST = [
+    new Spec("Anzahl", "x"),
+    new Spec("Leistung", "W"),
+    new Spec("Lumen", "lm"),
+    new Spec("Lux", "lx"),
+    new Spec("Effizienz", "lm/W"),
+    new Spec("PPF", "µmol/s"),
+    new Spec("PPFD", "µmol/(m²s)"),
+    new Spec("Effizienz", "µmol/J"),
+    new Spec("Preis", "€"),
+    new Spec("Stromk./Grow", "€"),
+    new Spec("Ausgb. n. 5 Grows", "€"),
+    new Spec("Treiber", "")
+];
+
+function Spec(name, unit) {
+    this.name = name;
+    this.unit = unit;
+}
+
 function calculateTableSpecs(product, version, ppfdInput, areaInput, usageInput) {
 
     let wattage = product["wattage"];
